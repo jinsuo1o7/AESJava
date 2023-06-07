dded0e8baf415bb5cd85b9246ee1eb85bf07b0d6
jinsuo1o7 <cil04110@gmail.com>
2023-06-07 14:47:07 +0900

    add AES password based alogrithm

--- src/test/java/org/example/AESExampleTest.java
+++ src/test/java/org/example/AESExampleTest.java
@@ -11,8 +11,7 @@ import javax.crypto.spec.IvParameterSpec;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.security.spec.InvalidKeySpecException;
 
 class AESExampleTest {
 
@@ -20,13 +19,29 @@ class AESExampleTest {
     void givenString_whenEncrypt_thenSuccess()
             throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException,
             BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException {
-
         String input = "drminside";
-        SecretKey key = AESExample.generateKey(128);
-        IvParameterSpec ivParameterSpec = AESExample.generateIv();
+        SecretKey key = AESUtil.generateKey(128);
+        IvParameterSpec ivParameterSpec = AESUtil.generateIv();
         String algorithm = "AES/CBC/PKCS5Padding";
-        String cipherText = AESExample.encrypt(algorithm, input, key, ivParameterSpec);
-        String plainText = AESExample.decrypt(algorithm, cipherText, key, ivParameterSpec);
+        String cipherText = AESUtil.encrypt(algorithm, input, key, ivParameterSpec);
+        String plainText = AESUtil.decrypt(algorithm, cipherText, key, ivParameterSpec);
         Assertions.assertEquals(input, plainText);
+    }
+
+    @Test
+    void givenPassword_whenEncrypt_thenSuccess()
+            throws InvalidKeySpecException, NoSuchAlgorithmException,
+            IllegalBlockSizeException, InvalidKeyException, BadPaddingException,
+            InvalidAlgorithmParameterException, NoSuchPaddingException {
+
+        String plainText = "www.baeldung.com";
+        String password = "baeldung";
+        String salt = "12345678";
+        IvParameterSpec ivParameterSpec = AESUtil.generateIv();
+        SecretKey key = AESUtil.getKeyFromPassword(password, salt);
+        String cipherText = AESUtil.encryptPasswordBased(plainText, key, ivParameterSpec);
+        String decryptedCipherText = AESUtil.decryptPasswordBased(
+                cipherText, key, ivParameterSpec);
+        Assertions.assertEquals(plainText, decryptedCipherText);
     }
 }
